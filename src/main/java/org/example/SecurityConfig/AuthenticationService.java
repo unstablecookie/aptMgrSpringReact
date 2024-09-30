e533f2838d1e1a9a988fe0d3931993366ed4d5da
Aleksey Melnikov <realxellor@gmail.com>
2024-10-01 00:49:55 +0300

    feat: auth controller tests

--- src/main/java/org/example/SecurityConfig/AuthenticationService.java
+++ src/main/java/org/example/SecurityConfig/AuthenticationService.java
@@ -1,43 +1,10 @@
 package org.example.SecurityConfig;
 
-import lombok.AllArgsConstructor;
-import org.example.user.RoleRepository;
-import org.example.user.UserRepository;
 import org.example.user.dto.UserAuthDto;
 import org.example.user.dto.UserDto;
-import org.example.user.dto.UserMapper;
-import org.example.user.model.Role;
-import org.example.user.model.User;
-import org.example.util.error.EntityNotFoundException;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+import org.example.util.auth.LoginResponse;
 
-import java.util.List;
-import java.util.Optional;
-
-@Service
-@AllArgsConstructor
-public class AuthenticationService {
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-
-    public UserDto signup(UserAuthDto userAuthDto) {
-        Role role = roleRepository.findById(1L).orElseThrow(
-                () -> new EntityNotFoundException(String.format("Role with id=%d was not found", 1L),
-                        "The required object was not found."));
-        User user = UserMapper.toUser(userAuthDto, passwordEncoder);
-        user.setRoles(List.of(role));
-        return UserMapper.toUserDto(userRepository.save(user));
-    }
-
-    public User authenticate(UserAuthDto userAuthDto) {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userAuthDto.getName(),
-                userAuthDto.getPassword()));
-        return userRepository.findByName(userAuthDto.getName())
-                .orElseThrow();
-    }
+public interface AuthenticationService {
+    UserDto signup(UserAuthDto userAuthDto);
+    LoginResponse authenticate(UserAuthDto userAuthDto);
 }
--- src/main/java/org/example/controller/AuthController.java
+++ src/main/java/org/example/controller/AuthController.java
@@ -2,22 +2,18 @@ package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.SecurityConfig.AuthenticationService;
-import org.example.SecurityConfig.JwtService;
 import org.example.user.dto.*;
-import org.example.user.model.User;
 import org.example.util.auth.LoginResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.stream.Collectors;
-
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private final JwtService jwtService;
+    
     private final AuthenticationService authenticationService;
     Logger logger = LoggerFactory.getLogger(AuthController.class);
 
@@ -33,13 +29,6 @@ public class AuthController {
     @ResponseStatus(HttpStatus.OK)
     public LoginResponse authenticate(@RequestBody UserAuthDto userAuthDto) {
         logger.info(String.format("user %s sign in", userAuthDto.getName()));
-        User authenticatedUser = authenticationService.authenticate(userAuthDto);
-        String jwtToken = jwtService.generateToken(authenticatedUser);
-        LoginResponse loginResponse = LoginResponse.builder()
-                .token(jwtToken)
-                .expiresIn(jwtService.getExpirationTime())
-                .roles(authenticatedUser.getRoles().stream().map(x -> x.getName().name()).collect(Collectors.toList()))
-                .build();
-        return loginResponse;
+        return authenticationService.authenticate(userAuthDto);
     }
 }
